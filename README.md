93070d04a049ce35cae0c01f859331a3f6668311
Steviegt6 <xxlennygamerxx@gmail.com>
2022-01-27 22:08:47 -0800

    Begin making this actually work.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # OpenMantleProxy
 Open-source alternative to the traditional Mantle program.
+
+## Functionality
+When ran, OpenMantleProxy starts a simple local HTTP server on an available port. It then modifies your `hosts` file to direct `s.optifine.net` requests to the newly-started server (this requires administrator privileges on Windows and root on *Nix).
+
+Now that the OpenMantleProxy server can actively receive OptiFine requests, it can reroute them as needed.
+
+### Modes
+There are three different proxy modes in OpenMantleProxy, detailed below:
+
+#### `mantle`
+`mantle` is the default mode, and reroutes all `s.optifine.net` requests to `capes.mantle.gg`. This is the same behavior as regular Mantle.
+
+#### `optimantle`
+`optimantle` initially seends an HTTP request to `s.optifine.net`. If a cape is not resolved, then an HTTP request is sent to `capes.mantle.gg`.
+
+#### `mantlefine`
+`mantlefine`, like `optimantle`, handles sending HTTP requests to `s.optifine.net` and `capes.mantle.gg`, but in reversed order.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use hudsucker::{
-    async_trait::async_trait,
-    certificate_authority::RcgenAuthority,
-    hyper::{Body, Request, Response},
-    *,
-};
 use argh::FromArgs;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+use std::net::TcpListener;
+use std::net::TcpStream;
 
 #[derive(FromArgs)]
 #[argh(description = "omp arguments")]
@@ -20,105 +18,154 @@ struct OmpArgs {
         switch,
         description = "provides help with proxy modes (see \"--mode\"). prints a simple message that describes proxy mode functionality."
     )]
-    mode_help: bool
+    mode_help: bool,
+
+    #[argh(
+        switch,
+        description = "cleans up the hosts file (removes any rerouting from s.optifine.net)",
+        short = 'c'
+    )]
+    clean: bool,
 }
 
-#[derive(Clone)]
-struct LogHandler {}
+fn main() {
+    println!("Reading launch arguments...");
 
-#[async_trait]
-impl HttpHandler for LogHandler {
-    async fn handle_request(
-        &mut self,
-        _ctx: &HttpContext,
-        req: Request<Body>,
-    ) -> RequestOrResponse {
-        println!("request");
-        println!("{:?}", req);
-        RequestOrResponse::Request(req)
-    }
-
-    async fn handle_response(&mut self, _ctx: &HttpContext, res: Response<Body>) -> Response<Body> {
-        println!("response");
-        println!("{:?}", res);
-        res
-    }
-}
-
-#[tokio::main]
-async fn main() {
-    println!("Welcome to OpenMantleProxy!");
-    println!("This is a Free/Libre proxy to recreate Mantle's functionality, fully transparent.");
-
-    let args:OmpArgs = argh::from_env();
-
-    println!("Read launch arguments.");
+    let args: OmpArgs = argh::from_env();
 
     if args.mode_help {
         println!(
-"Proxy modes:
+            "\n\nProxy modes:
  - mantle: Default proxy configuration, displays only only Mantle capes.
  - optimantle: Displays Mantle capes if an OptiFine cape could not be resolved.
- - mantlefine: Displays OptiFine capes if a Mantle cape could not be resolved."
+ - mantlefine: Displays OptiFine capes if a Mantle cape could not be resolved.\n\n"
         );
 
         return;
     }
 
-    let mode:String;
+    if args.clean {
+        println!("\n\nCleaning up the hosts file...\n\n");
+        let clean_hosts_path = resolve_hosts_path();
+        clean_hosts(&clean_hosts_path, None);
+        return;
+    }
+
+    let proxy_mode: String;
 
     if args.mode.is_none() {
-        println!("Could not resolve argument \"--mode\" (\"-m\")");
-        println!("Setting mode to default (\"mantle\")");
-        mode = "mantle".to_string();
+        println!("Could not resolve argument: \"--mode\" (\"-m\")");
+        println!("Setting mode to default: \"mantle\"");
+        proxy_mode = "mantle".to_string();
+    } else {
+        proxy_mode = args.mode.unwrap();
     }
-    else {
-        mode = args.mode.unwrap();
+
+    println!(
+        "
+      .+=:.   .:=+.      
+      +*****+*****+      
+     -***=+***+=***-     
+    .***=       =***.    
+    +***         ***+    
+   -***:         :***-   OpenMantleProxy v1.0.0
+  .***=           =***.  by the Uranometrical Team
+  +**+  .       .  ***+  
+ =***.:+*=.   .=*+::***- 
+.***++*****=:=*****++***.
+:+*****=:+*****+:=*****+:
+  :++=.   :=+=:   .=++:  
+    "
+    );
+    println!("\nUsing proxy mode: {}", proxy_mode);
+
+    let mut port: u16 = 3764;
+
+    while !port_scanner::local_port_available(port) {
+        port += 1;
     }
 
-    println!("Using proxy mode: {}", mode);
+    println!("Resolved open port: {}", port);
 
-    println!("Parsing certificate.");
+    let listener =
+        TcpListener::bind(format!("127.0.0.1:{}", port)).expect("Failed to bind to TcpListener.");
 
-    let mut cert_bytes:&[u8] = include_bytes!("../certs/cert.pem");
+    println!("Binded to: 127.0.0.1: {}\n", port);
 
-    let cert = rustls::Certificate(
-        rustls_pemfile::certs(&mut cert_bytes)
-            .expect("Failed to parse certificate, aborting")
-            .remove(0)
+    let hosts_path = resolve_hosts_path();
+    clean_hosts(
+        &hosts_path,
+        Some(format!("127.0.0.1:{} s.optifine.net", port).to_owned()),
     );
 
-    println!("Parsing key.");
-
-    let mut key_bytes:&[u8] = include_bytes!("../certs/key.pem");
-
-    let key = rustls::PrivateKey(
-        rustls_pemfile::pkcs8_private_keys(&mut key_bytes)
-            .expect("Failed to parse key, aborting")
-            .remove(0)
-    );
-    println!("Creating certificate authority.");
-    let ca:RcgenAuthority = RcgenAuthority::new(
-        key, cert, 1_000
-    ).expect("Failed to create certificate authority");
-
-    println!("Building proxy.");
-    let proxy = ProxyBuilder::new()
-        .with_addr(std::net::SocketAddr::from(([127, 0, 0, 1], 443)))
-        .with_rustls_client()
-        .with_ca(ca)
-        .with_http_handler(LogHandler {})
-        .build();
-
-    println!("Starting proxy.");
-
-    if let Err(e) = proxy.start(shutdown_signal()).await {
-        println!("{}", e);
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => match handle(stream) {
+                Err(e) => eprintln!("Error handling incoming request: {}", e),
+                _ => {}
+            },
+            Err(e) => eprintln!("Connection failed: {}", e),
+        }
     }
 }
 
-async fn shutdown_signal() {
+fn resolve_hosts_path() -> String {
+    let mut hosts_path: String = String::new();
+
+    if cfg!(windows) {
+        // TODO: This is terrinly hard-coded. Fix... eventually?
+        hosts_path = r"C:\Windows\System32\Drivers\etc\hosts".to_owned();
+
+        println!("Presumably running on Windows.");
+    } else if cfg!(unix) {
+        hosts_path = "/etc/hosts".to_owned();
+
+        println!("Presumably running on *Nix (MacOS, Linux, etc.)");
+    }
+
+    println!("Current hosts path: {}", hosts_path);
+
+    while !std::path::Path::new(&hosts_path).exists() {
+        println!("Hosts file path could not be resolved, please input the path manually:");
+        hosts_path = text_io::read!("{}\n");
+    }
+
+    hosts_path
+}
+
+fn clean_hosts(hosts_path: &String, additional_line: Option<String>) {
+    let input =
+        File::open(hosts_path).expect("Could not open hosts, is the process lacking permission?");
+    let buffered = BufReader::new(input);
+    let mut lines = std::vec::Vec::new();
+
+    for line in buffered.lines() {
+        let s = line.expect("Couldn't get line as string, what?");
+
+        if !s.ends_with("s.optifine.net") {
+            lines.push(s);
+        }
+    }
+
+    if additional_line.is_some() {
+        lines.push(additional_line.unwrap());
+    }
+
+    write!(
+        File::create(hosts_path).expect("Could not open hosts, is the process lacking permission?"),
+        "{}",
+        format!("{}", lines.join("\n"))
+    );
+}
+
+fn handle(stream: TcpStream) -> std::io::Result<()> {
+
+    
+    Ok(())
+}
+
+/*async fn shutdown_signal() {
     tokio::signal::ctrl_c()
     .await
     .expect("Failed to install CTRL+C signal handler");
-}
+}*/
