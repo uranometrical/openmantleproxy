b0a9b5345743697868f574be9b2ab43c9f11ea57
Steviegt6 <xxlennygamerxx@gmail.com>
2022-02-12 13:32:42 -0800

    Implement hosts file modification API, resolves #1.

---
+++ src/Uranometrical.OpenMantleProxy.Hosts/HostsFileEditor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Uranometrical.OpenMantleProxy.Hosts
+{
+    /// <summary>
+    ///     Disposable object that handles editing a system's <c>hosts</c> file.
+    /// </summary>
+    public class HostsFileEditor : IDisposable
+    {
+        /// <summary>
+        ///     An <see cref="Action"/> (<see cref="Delegate"/>) allowing a user to interface with <see cref="HostsFileEditor"/> to enable file writing.
+        /// </summary>
+        public delegate void HostsFileWriter(StreamWriter writer, string[] contents);
+        
+        protected readonly FileInfo HostsFile;
+        protected readonly FileStream HostsStream;
+        protected readonly StreamWriter HostsWriter;
+        protected readonly string[] Contents;
+        
+        /// <summary>
+        ///     Creates a new <see cref="HostsFileEditor"/> instance.
+        /// </summary>
+        /// <param name="hostsFile">The <see cref="FileInfo"/> pointing to the system's <c>hosts</c> file.</param>
+        public HostsFileEditor(FileInfo hostsFile)
+        {
+            HostsFile = hostsFile;
+            HostsStream = new FileStream(hostsFile.ToString(), FileMode.Open);
+            HostsWriter = new StreamWriter(HostsStream);
+            
+            string? line;
+            List<string> lines = new();
+            using StreamReader reader = new(HostsStream);
+            
+            while ((line = reader.ReadLine()) != null)
+                lines.Add(line);
+            
+            Contents = lines.ToArray();
+        }
+        
+        /// <summary>
+        ///     Creates a new <see cref="HostsFileEditor"/> instance.
+        /// </summary>
+        /// <param name="hostsFile">A <see cref="string"/> input which is used to construct a <see cref="FileInfo"/>.</param>
+        public HostsFileEditor(string hostsFile) : this(new FileInfo(hostsFile))
+        {
+        }
+
+        /// <summary>
+        ///     Creates a new <see cref="HostsFileEditor"/> instance which automatically resolves a <c>hosts</c> file's location.
+        /// </summary>
+        public HostsFileEditor() : this(HostsFileResolver.ResolveBlocking())
+        {
+        }
+
+        /// <summary>
+        ///     Handles writing to the <c>hosts</c> file.
+        /// </summary>
+        /// <param name="fileWriter"></param>
+        public virtual void Write(HostsFileWriter fileWriter) => fileWriter(HostsWriter, Contents);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing)
+                return;
+            
+            HostsWriter.Flush();
+            HostsWriter.Dispose();
+            
+            HostsStream.Flush();
+            HostsStream.Dispose();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ src/Uranometrical.OpenMantleProxy.Hosts/HostsFileResolver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Uranometrical.OpenMantleProxy.Hosts
+{
+    /// <summary>
+    ///     Resolves the location of a computer's <c>hosts</c> file.
+    /// </summary>
+    public static class HostsFileResolver
+    {
+        /// <summary>
+        ///     Identical to <see cref="Resolve"/>, but requires user input in the event of a failed resolution.
+        /// </summary>
+        /// <returns>A <see cref="FileInfo"/> pointing to the <c>hosts</c> file.</returns>
+        public static FileInfo ResolveBlocking()
+        {
+            FileInfo file = Resolve();
+
+            while (!file.Exists)
+            {
+                Console.WriteLine("Could not resolve hosts file, please input the location manually:");
+                file = new FileInfo(Console.ReadLine() ?? "");
+            }
+
+            return file;
+        }
+
+        /// <summary>
+        ///     Resolves the default <c>hosts</c> file location on *Nix and Windows.
+        /// </summary>
+        /// <returns>A <see cref="FileInfo"/> pointing to the <c>hosts</c> file.</returns>
+        public static FileInfo Resolve() =>
+            Environment.OSVersion.Platform == PlatformID.Unix ? ResolveUnix() : ResolveWindows();
+
+        /// <summary>
+        ///     Resolves the <c>hosts</c> file location on Windows systems.
+        /// </summary>
+        /// <returns>A <see cref="FileInfo"/> pointing to the <c>hosts</c> file.</returns>
+        public static FileInfo ResolveWindows()
+        {
+            // https://github.com/uranometrical/openmantleproxy/issues/1
+            return new FileInfo((string) Registry.GetValue(
+                @"\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters",
+                "DataBasePath",
+                @"C:\Windows\System32\Drivers\etc\hosts"
+            )!);
+        }
+
+        /// <summary>
+        ///     Resolves the <c>hosts</c> file location on *Nix systems.
+        /// </summary>
+        /// <returns>A <see cref="FileInfo"/> pointing to the <c>hosts</c> file.</returns>
+        public static FileInfo ResolveUnix() => new("/etc/hosts");
+    }
+}
