65d2978d6262649aeb7cdd221ba100809b5be99f
Steviegt6 <xxlennygamerxx@gmail.com>
2022-02-12 15:12:47 -0800

    Finish server implementation.

--- src/Uranometrical.OpenMantleProxy/Commands/ProxyServerCommand.cs
+++ src/Uranometrical.OpenMantleProxy/Commands/ProxyServerCommand.cs
@@ -6,6 +6,8 @@ using CliFx;
 using CliFx.Attributes;
 using CliFx.Extensibility;
 using CliFx.Infrastructure;
+using Uranometrical.OpenMantleProxy.Hosts;
+using Uranometrical.OpenMantleProxy.Server;
 
 namespace Uranometrical.OpenMantleProxy.Commands
 {
@@ -34,16 +36,19 @@ namespace Uranometrical.OpenMantleProxy.Commands
             }
         }
 
-        public const string Description = "Starts a proxy server to redirect any and all s.optifine.net HTTP requests." +
-                                          "\n                    Also modifies the \"hosts\" file to handle redirecting." +
-                                          "\n                    Run the \"clean\" command after closing this to reset.";
+        public const string Description =
+            "Starts a proxy server to redirect any and all s.optifine.net HTTP requests." +
+            "\n                    Also modifies the \"hosts\" file to handle redirecting." +
+            "\n                    Run the \"clean\" command after closing this to reset.";
+
+        public const string OptiFineIp = "http://107.182.233.85";
 
         [CommandOption("mode", 'm', Description = "The proxy mode to use.", Converter = typeof(ProxyModeConverter))]
         public ProxyMode Mode { get; init; } = ProxyMode.Mantle;
 
         [CommandOption("servers", 's', Description = "The servers to use. Only used if \"mode\" is set to Custom.")]
-        public string[] Servers { get; init; } = Array.Empty<string>();
-        
+        public string[] Servers { get; private set; } = Array.Empty<string>();
+
         public async ValueTask ExecuteAsync(IConsole console)
         {
             await console.Output.WriteLineAsync(
@@ -63,6 +68,55 @@ namespace Uranometrical.OpenMantleProxy.Commands
             ");
 
             await console.Output.WriteLineAsync("Using proxy mode: " + Mode);
+
+            if (Mode == ProxyMode.Custom)
+                await console.Output.WriteLineAsync("Custom servers: " + string.Join(", ", Servers));
+
+            await console.Output.WriteLineAsync("\nModifying hosts file...");
+
+            new HostsFileEditor().Write(contents =>
+            {
+                List<string> content = contents.Where(x => !x.EndsWith(" s.optifine.net")).ToList();
+                content.Add("127.0.0.1 s.optifine.net");
+                return content;
+            });
+
+            await console.Output.WriteLineAsync("Modified hosts file!");
+
+            switch (Mode)
+            {
+                case ProxyMode.Mantle:
+                    Servers = new[] {"http://capes.mantle.gg"};
+                    break;
+
+                case ProxyMode.OptiMantle:
+                    Servers = new[] {OptiFineIp, "http://capes.mantle.gg"};
+
+                    break;
+
+                case ProxyMode.MantleFine:
+                    Servers = new[] {"http://capes.mantle.gg", OptiFineIp};
+
+                    break;
+
+                case ProxyMode.Custom:
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(Mode));
+            }
+
+
+            await console.Output.WriteLineAsync("Starting server! You can kill the server by pressing <ENTER>.");
+
+            CapeServer server = new(Servers);
+            server.Listen();
+
+            await console.Output.WriteLineAsync("Killed server! Reverting hosts file...");
+
+            new HostsFileEditor().Write(contents => contents.Where(x => !x.EndsWith(" s.optifine.net")));
+
+            await console.Output.WriteLineAsync("Operations completed.!");
         }
     }
 }
---
+++ src/Uranometrical.OpenMantleProxy/Server/CapeServer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uranometrical.OpenMantleProxy.Server
+{
+    public class CapeServer
+    {
+        protected readonly HttpListener Listener = new();
+        protected readonly HttpClient Client = new();
+        protected readonly string[] Servers;
+
+        public CapeServer(params string[] servers)
+        {
+            // We want to listen to port 80 as that's the default HTTP port (used for s.optifine.net).
+            Listener.Prefixes.Add("http://127.0.0.1:80/");
+            Listener.Prefixes.Add("http://localhost:80/");
+
+            Client.DefaultRequestHeaders.Add("User-Agent", "OpenMantleProxy Cape Server Client");
+
+            Servers = servers;
+        }
+
+        public virtual void Listen()
+        {
+            Listener.Start();
+
+            while (true)
+            {
+                HttpListenerContext context = Listener.GetContext();
+                Console.WriteLine(context.Request.RawUrl);
+
+                byte[] buf = ResolveCape(context.Request.RawUrl ?? "");
+
+                HttpListenerResponse response = context.Response;
+                response.ContentLength64 = buf.Length;
+                response.OutputStream.Write(buf);
+
+                context.Response.Close();
+            }
+        }
+
+        protected virtual byte[] ResolveCape(string input)
+        {
+            foreach (string server in Servers)
+            {
+                HttpResponseMessage response = Client.GetAsync(server + input).Result;
+
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                    continue;
+
+                if (response.Content.ReadAsStringAsync().Result.Contains("Not Found"))
+                    continue;
+
+                if (response.Content.Headers.ContentType?.ToString() != "image/png")
+                    continue;
+
+                using Stream stream = response.Content.ReadAsStream();
+                return stream is not MemoryStream memory ? Encoding.UTF8.GetBytes("404 uh oh") : memory.ToArray();
+            }
+
+            return Encoding.UTF8.GetBytes("404 uh oh");
+        }
+    }
+}
